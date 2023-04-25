3971dcb323ef901456a1424faf5ae762d00f8f6f
Ruperth <ruperthnyagesoa@gmail.com>
2023-04-25 10:55:46 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,25 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return <React.Fragment>
+  <div>
+    <NavBar />
+    <Switch>
+    <Route path="/movies">
+        <Movies />
+      </Route>
+      <Route path="/directors">
+        <Directors />
+      </Route>
+      <Route path="/actors">
+        <Actors />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </div>
+</React.Fragment>
 }
 
 export default App;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,23 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const movieDetails = movies.map((movie) => (
+    <div key={movie.title}>
+      <h2>{movie.title}</h2>
+      <p>Runtime: {movie.time} min.</p>
+      <ul>
+        {movie.genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+  return <React.Fragment>
+    <div>
+      <h1>Movies Page</h1>
+      {movieDetails}
+    </div>
+  </React.Fragment>
 }
 
 export default Movies;
